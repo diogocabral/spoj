eea68b01a11a870c9ecd34e9ece44d44e04f27b5
Diogo Cabral de Almeida <diogo.cabral@gmail.com>
2015-07-08 19:52:48 -0300

    PRIME1 problem solution.

---
+++ PRIME1/prime.py
@@ -0,0 +1,36 @@
+# import time
+
+# start_time = time.time()
+
+primes = [2]
+
+def is_prime(number):
+	if number <= 1:
+		return False
+
+	for prime in primes:		
+		if prime * prime > number:
+			break
+		if number % prime == 0:
+			return False
+
+	return True
+
+for number in xrange(1, 31623, 2):
+	if is_prime(number):
+		primes.append(number)
+
+total_of_test_cases = input()
+
+for t in xrange(total_of_test_cases):
+	m,n = raw_input().split(" ")
+	
+	m = int(m)
+	n = int(n)
+
+	for number in xrange(m, n + 1):
+		if (is_prime(number)):
+			print number
+	print
+
+# print("--- %s seconds ---" % (time.time() - start_time))
